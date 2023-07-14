263d9ff5fa498033fb859e95f59907244f626349
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2023-07-14 21:53:36 +0100

    Update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - entry of the program
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	char letters[26] = "abcdefghijklmnopqrstuvwxyz";
+	int i;
+
+	for (i = 0; i < 26; i++)
+	{
+		putchar(letters[i]);
+	}
+	putchar('\n');
+	return (0);
+}
