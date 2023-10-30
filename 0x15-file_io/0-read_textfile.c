5154a2c8c12e516cf0370e8abfba381ff05a0982
hmoustaphaousmane <hmoustaphaousmane01@gmail.com>
2023-10-30 08:32:30 +0100

    Tread lightly, she is near

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * read_textfile - Function that reads a text file and prints it to the POSIX
+ * standard output
+ *
+ * @filename: Name of the file to be red
+ * @letters: Number of letters the function should read and print
+ *
+ * Return: Actual number of letters red and printed (Success), 0 (Failure)
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	ssize_t file, rfile, wfile;
+	char *buffer;
+
+	if (filename == NULL)
+		return (0);
+
+	file = open(filename, O_RDONLY);
+	rfile = read(file, buffer, letters);
+	wfile = write(STDOUT_FILENO, buffer, rfile);
+
+	if (file == -1 || rfile == -1 || wfile != rfile)
+	{
+		free(buffer);
+		return (0);
+	}
+
+	free(buffer);
+	close(file);
+
+	return (wfile);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,4 @@
+# 0x15-file\_io
+
+* 0-read\_textfile.c
+
